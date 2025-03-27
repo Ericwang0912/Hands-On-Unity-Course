c32f1c2691f85f8e1f2f88577a8cd30b4de1215e
Ericwang0912 <98420182+Ericwang0912@users.noreply.github.com>
2025-03-27 00:46:43 -0400

    Added Chapter 12, especially the PP Volume

---
+++ Chapter 13/Scripts/PlayerShooting.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject shootPoint;
+    public ParticleSystem muzzleEffect;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void OnFire(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            GameObject clone = Instantiate(prefab);
+
+            clone.transform.position = shootPoint.transform.position;
+            clone.transform.rotation = shootPoint.transform.rotation;
+
+            muzzleEffect.Play();
+        }
+    }
+}
